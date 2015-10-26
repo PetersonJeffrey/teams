25dcc652617c8adf0f213b8e40290f2ec7c8f1ed
Jeff Peterson <peterson.jeffrey@gmail.com>
2015-10-26 18:01:59 -0500

    Add domain objects and repository interfaces

--- src/main/java/learningspringboot/TeamsApplication.java
+++ src/main/java/learningspringboot/TeamsApplication.java
@@ -1,12 +1,35 @@
 package learningspringboot;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import javax.annotation.PostConstruct;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan
+@EnableAutoConfiguration
 @SpringBootApplication
 public class TeamsApplication {
 
+    private static final Logger log = LoggerFactory.getLogger(TeamsApplication.class);
+
     public static void main(String[] args) {
         SpringApplication.run(TeamsApplication.class, args);
+    }
+
+    @Autowired
+    TeammateRepository teammateRepository;
+
+    @PostConstruct
+    void seeTheRoster() {
+        for (Teammate teammate : teammateRepository.findAll()) {
+            log.info(teammate.toString());
+        }
     }
 }
